e69778bf91ae015aafad74c38437450ccdd02e1a
Greifrut <artur.bunko@gmail.com>
2017-07-17 09:18:49 +0300

    add clients db

--- app.rb
+++ app.rb
@@ -6,6 +6,11 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:barbershop.db"
 
+#создаем сущность
+
+class Client < ActiveRecord::Base
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
